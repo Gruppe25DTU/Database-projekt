2b79f41796617600ab8d4dbe5199166d1e7e215b
lasse <lassejensen213@gmail.com>
2017-03-14 10:06:09 +0100

    Importing

--- Database projekt/src/connector01917/Constant.java
+++ Database projekt/src/connector01917/Constant.java
@@ -7,7 +7,7 @@ public abstract class Constant
 {
 	public static final String
 	server					= "sql11.freemysqlhosting.net",  // database-serveren
-	database				=  "sql11162519",  //"jdbcdatabase", // navnet paa din database = dit studienummer
+	database				= "sql11162519",  //"jdbcdatabase", // navnet paa din database = dit studienummer
 	username				= "sql11162519", // dit brugernavn = dit studienummer 
 	password				= "5JSFwhTFgl"; // dit password som du har valgt til din database
 
